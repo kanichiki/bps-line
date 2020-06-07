f6a7d94f91b201cab0ca6c36268a5c3879daebfd
Ayumu <ayumu.bete@gmail.com>
2020-06-07 19:50:17 +0900

    pythonでの開発テスト

--- python/main.py
+++ python/main.py
@@ -8,6 +8,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import flask
 from flask import Flask, request, abort
 import os
 from os.path import join,dirname
@@ -27,6 +28,7 @@ LINE_CHANNEL_SECRET = os.environ.get("channelSecret")
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
+
 @app.route('/rollcall',methods=["POST"])
 def index():
     data = request.data.decode('utf-8')
@@ -36,7 +38,44 @@ def index():
     reply = '参加受付を終了します\n\n参加者は\n\n%sさん\n\nです！' % display_names
 
     line_bot_api.reply_message(str(data["replyToken"]),TextSendMessage(text=reply))
-    return "hello"
+    return flask.redirect(flask.url_for("night"))
+
+# 夜（能力行使。ただし、初日は人狼による襲撃はない）
+@app.route('/night')
+def night() -> None:
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    reply = '夜になりました。みなさん'
+
+    line_bot_api.reply_message(str(data["replyToken"]),TextSendMessage(text=reply))
+    
+# 朝（襲撃の結果表示 -> 話し合い）
+@app.route('/morning')
+def night() -> None:
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    reply = '朝になりました。昨晩の犠牲者はあゆむさんです。話し合ってください。'
+
+    line_bot_api.reply_message(str(data["replyToken"]),TextSendMessage(text=reply))
+
+# 昼（投票）
+@app.route('/noon')
+def night() -> None:
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    reply = '昼になりました。みなさん投票してください。'
+
+    line_bot_api.reply_message(str(data["replyToken"]),TextSendMessage(text=reply))
+
+# 夕方（処刑）
+@app.route('/evening')
+def night() -> None:
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    reply = '夕方になりました。処刑される人はあゆむさんです'
+
+    line_bot_api.reply_message(str(data["replyToken"]),TextSendMessage(text=reply))
+
 
 if __name__ == '__main__':
     app.debug = True
--- python/rollcall.py
+++ python/rollcall.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from os.path import join,dirname
+from os.path import join, dirname
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
@@ -8,9 +8,11 @@ load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
+
 def get_connection():
     dsn = os.environ.get('DATABASE_URL')
     return psycopg2.connect(dsn)
+
 
 def rollcall(pl_id):
     with get_connection() as conn:
