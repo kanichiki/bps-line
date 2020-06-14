1dce96e1e2f83ab011df4f9160f12b37dfd8f7fd
kanichiki <kansei.ichiki@gmail.com>
2020-06-14 23:14:00 +0900

    投票結果をフレックスに

--- template/messages/crazy_noisy/replyExecutorIsNotGuru.js
+++ template/messages/crazy_noisy/replyExecutorIsNotGuru.js
@@ -3,8 +3,33 @@ const parts = require("../constants/messageParts");
 exports.main = async (executorDisplayName) => {
     return [
         {
-            type: "text",
-            text: `${executorDisplayName}さんは教祖ではなかったようですが、拷問の結果気が狂ってしまいました`
+            "type": "flex",
+            "altText": "役職確認",
+            "contents": {
+                "type": "bubble",
+                "body": {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "wrap": true,
+                      "contents": [
+                        {
+                          "type": "span",
+                          "text": executorDisplayName,
+                          "color": parts.mainColor
+                        },
+                        {
+                          "type": "span",
+                          "text": "さんは教祖ではなかったようですが、拷問の結果気が狂ってしまいました"
+                        }
+                      ],
+                      "size": "lg"
+                    }
+                  ]
+                }
+              }
         }
     ]
 }
